021b6da7572d661caa94f3d265c80b5831c90040
Naufal Nasrullah <naufaln176@gmail.com>
2025-01-12 09:45:52 +0700

    feat(login-page): complete login page

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,8 +1,48 @@
 import { Layout } from "@/components/layout";
 import { InputType, Input } from "@/components/ui/input";
-import { Link } from "react-router-dom";
+import { axiosInstance } from "@/lib/utils";
+import { ResponseAPI } from "@/types/response-api";
+import { useMutation } from "@tanstack/react-query";
+import { AxiosError } from "axios";
+import { useForm } from "react-hook-form";
+import { Link, useNavigate } from "react-router-dom";
+
+type LoginInputType = {
+  email: string;
+  password: string;
+};
+
+
 
 function LoginPage() {
+  const navigate = useNavigate();
+
+  const { mutate, isPending } = useMutation({
+    mutationFn: async (data: LoginInputType) =>
+      await axiosInstance.post("/admin/login", data),
+    onSuccess: () => {
+      navigate("/");
+    },
+    onError: (error: AxiosError<ResponseAPI>) => {
+      alert(error?.response?.data.message);
+    },
+  });
+
+  const {
+    register,
+    handleSubmit,
+    // formState: { errors },
+  } = useForm<LoginInputType>({
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  const onSubmit = (data: LoginInputType) => {
+    console.log(data);
+    mutate(data);
+  };
 
   return (
     <Layout
@@ -10,18 +50,30 @@ function LoginPage() {
       className="grid h-[calc(100svh-var(--header-height-sm)-var(--footer-height))] min-h-[540px] place-items-center sm:h-[calc(100svh-var(--header-height-lg)-var(--footer-height))]"
     >
       <div className="mx-auto w-full max-w-[350px] py-5">
-        <form className="flex w-full flex-col items-center gap-8 rounded-lg border-2 border-blue-400 bg-gradient-to-br from-blue-300 via-cyan-300 to-sky-300 pb-10 pt-10">
+        <form
+          onSubmit={handleSubmit(onSubmit)}
+          className="flex w-full flex-col items-center gap-8 rounded-lg border-2 border-blue-400 bg-gradient-to-br from-blue-300 via-cyan-300 to-sky-300 pb-10 pt-10"
+        >
           <h1 className="pb-4 text-center text-3xl font-semibold">Login</h1>
           {input.map((item) => (
-            <div className="flex flex-col gap-1">
-              <Input key={item.inputFor} {...item} />
+            <div key={item.inputFor} className="flex flex-col gap-1">
+              <Input
+                {...register(item.inputFor as keyof LoginInputType)}
+                {...item}
+              />
             </div>
           ))}
           <button
             className="w-64 rounded-lg bg-blue-400 py-2 text-lg font-semibold text-white disabled:opacity-75"
-            // disabled={isPending}
+            disabled={isPending}
           >
-            Login
+            {isPending ? (
+              <span className="flex items-center justify-center">
+                <div className="mr-3 size-7 animate-spin rounded-full border-b-2 border-t-2 border-white" />
+              </span>
+            ) : (
+              "Login"
+            )}
           </button>
           <p className="mt-8">
             don't have account?{" "}
@@ -36,18 +88,18 @@ function LoginPage() {
 }
 
 const input: InputType[] = [
-    {
-      inputFor: "email",
-      type: "email",
-      placeholder: "jhondoe@gmail.com",
-      required: true,
-    },
-    {
-      inputFor: "password",
-      type: "password",
-      placeholder: "password",
-      required: true,
-    },
-  ];
+  {
+    inputFor: "email",
+    type: "email",
+    placeholder: "jhondoe@gmail.com",
+    required: true,
+  },
+  {
+    inputFor: "password",
+    type: "password",
+    placeholder: "password",
+    required: true,
+  },
+];
 
 export default LoginPage;
